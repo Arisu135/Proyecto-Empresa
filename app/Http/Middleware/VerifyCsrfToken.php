48192d2779af80fe2b3ae34e989ab599d41b7cb5
Arisu135 <rez037903@gmail.com>
2025-10-24 17:46:48 -0500

    Revert: Revertido el cambio de CSRF. Procediendo a limpieza profunda para error 505.

--- app/Http/Middleware/VerifyCsrfToken.php
+++ app/Http/Middleware/VerifyCsrfToken.php
@@ -11,12 +11,5 @@ class VerifyCsrfToken extends Middleware
      *
      * @var array<int, string>
      */
-    protected $except = [
-        // 🛑 AÑADE ESTAS RUTAS PARA LA PRUEBA DIAGNÓSTICA DEL 505 🛑
-        '/carrito/agregar/*',
-        '/pedido/resumen', // Si es una ruta POST, inclúyela
-        '/pedido/finalizar', // Si es una ruta POST, inclúyela
-        // Puedes usar comodines para cubrir todas las rutas del carrito
-        '/carrito/*'
-    ];
+    
 }
